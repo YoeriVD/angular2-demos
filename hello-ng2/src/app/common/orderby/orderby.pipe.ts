4f3cebad1e0750e68069fee9bee9ff3d658bedb9
YoeriVD <yoeri.vandamme@gmail.com>
2017-02-19 13:28:53 +0100

    betere plaats voor log

--- hello-ng2/src/app/common/orderby/orderby.pipe.ts
+++ hello-ng2/src/app/common/orderby/orderby.pipe.ts
@@ -8,8 +8,8 @@ import { Pipe, PipeTransform } from '@angular/core';
 export class OrderByPipe implements PipeTransform {
     transform(value: any[], ...args: any[]): any {
         if(!value) return value;
+        console.debug("sorting on", args[0]);
         return value.sort((current, next) =>{
-            console.log("sorting on", args[0]);
             const currentProperty = current[args[0]];
             const nextProperty = next[args[0]];
             if(currentProperty < nextProperty) return -1;
