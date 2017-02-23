b5d7ffefe2993e92fd6310e92ee673c0c316a720
YoeriVD <yoeri.vandamme@gmail.com>
2017-02-23 13:08:08 +0100

    added zoom directive

--- hello-ng2-sm/src/app/common/common.module.ts
+++ hello-ng2-sm/src/app/common/common.module.ts
@@ -1,13 +1,14 @@
 import { NgModule } from '@angular/core';
 
 import { OrderByPipe } from './order-by.pipe';
-import {FilterPipe } from './filter.pipe'
-import { CurrencySignComponent} from './currency-sign.component'
+import { FilterPipe } from './filter.pipe'
+import { CurrencySignComponent } from './currency-sign.component'
+import { ZoomDirective } from './zoom.directive'
 
 @NgModule({
     imports: [],
-    exports: [OrderByPipe, FilterPipe, CurrencySignComponent],
-    declarations: [OrderByPipe, FilterPipe, CurrencySignComponent],
+    exports: [OrderByPipe, FilterPipe, CurrencySignComponent, ZoomDirective],
+    declarations: [OrderByPipe, FilterPipe, CurrencySignComponent, ZoomDirective],
     providers: [],
 })
 export class CommonModule { }
---
+++ hello-ng2-sm/src/app/common/zoom.directive.ts
@@ -0,0 +1,15 @@
+import { Directive, HostListener, ElementRef, Input } from '@angular/core'
+
+@Directive({
+    selector: '[zoom]'
+})
+export class ZoomDirective {
+    @Input() zoom : number;
+    constructor(private element : ElementRef){}
+    @HostListener("mouseenter") onMouseEnter() {
+        this.element.nativeElement.style.transform = `scale(${this.zoom || 1.4 })`;
+    }
+    @HostListener("mouseleave") onMouseLeave() {
+        this.element.nativeElement.style.transform = '';
+    }
+}
